1256237eb25812bb65f92e67840a55783c1e4a25
Tanay <tanay@webexposure.ca>
2016-10-15 17:10:20 -0400

    Parsing of device date announced and device status.

--- BackEnd/dataRetrieval.php
+++ BackEnd/dataRetrieval.php
@@ -4,7 +4,7 @@
 
 	class dataRetrieval {
 		public static function fetchDevices() {
-			
+
 			$fonoapi = fonoApi::init(configuration::$apiKey);
 
 			try {
@@ -20,9 +20,6 @@
 		}
 
 		public static function scanDevice($device) {
-			if (!empty($device->DeviceName))    echo "Device : ". $device->DeviceName . "<br>";
-			if (!empty($device->announced))         echo "announced : ". $device->announced . "<br>";
-			if (!empty($device->status))         echo "status : ". $device->status . "<br>";
 
 			if (self::devicePreviouslyScanned($device)) //return;
 			{ echo "scanned previously"; } else { echo "not scanned"; }
@@ -61,8 +58,14 @@
 		public static function devicePreviouslyScanned($device) {
 			// Creates a PDO statement and binds the appropriate parameters
 			$db = configuration::getConnection();
-			//$date = '10, 30, 2016';
-			$date = null;
+
+			if (!self::deviceAvailable($device->status)) return true;
+
+			if (!empty($device->DeviceName))    echo "Device : ". $device->DeviceName . "<br>";
+			if (!empty($device->announced))         echo "announced : ". $device->announced . "<br>";
+			if (!empty($device->status))         echo "status : ". $device->status . "<br>";
+
+			$date = self::getDateAnnounced($device->announced);
 
 			$sql = self::build_query([
 					[               "SELECT COUNT(*) FROM htbap.devices_scanned WHERE device_name = :name"],
@@ -72,28 +75,48 @@
 
 			$device_count_stmt = $db->prepare($sql);
 			$device_count_stmt->bindParam(':name', $device->DeviceName, PDO::PARAM_INT);
+
 			// Optional bindings.
 			$date &&       $device_count_stmt->bindValue(':date_announced', $date, \PDO::PARAM_STR);
 			$device->status &&    $device_count_stmt->bindValue(':status', $device->status, \PDO::PARAM_STR);
 
-
-
-			//$query = "SELECT device_id FROM devices_scanned WHERE device_name IS NULL";
-			/*$result = $db->prepare($query);
-			//$result ->bindParam(':p', $q, PDO::PARAM_INT);
-			$result->execute();
-			$rows = $result->rowCount();*/
-			$res = $device_count_stmt->execute();
-
-			// Executes query
-			if ($res) {
-				// Ensures specification of that search level exist
-				//echo '<br>' . $device_count_stmt->fetchColumn() . '<br>';
-				echo 'success: '. $device_count_stmt->fetchColumn(). '   ';
+			// Executes query & returns whether entries matching the description are already in db
+			if ($device_count_stmt->execute()) {
 				return (($device_count_stmt->fetchColumn()) > 0);
 			} else {
-
 				echo '<br>' . "query failed" . '<br>';
+				return false;
+			}
+		}
+
+		static function deviceAvailable($status) {
+			return !((strpos($status, "Coming soon") !== false) || (strpos($status, "Rumored") !== false));
+		}
+
+		static function getDateAnnounced($rawDate) {
+			try {
+				$txt = $rawDate;
+				$re1='.*?';	# Non-greedy match on filler
+			 $re2='((?:(?:[1]{1}\\d{1}\\d{1}\\d{1})|(?:[2]{1}\\d{3})))(?![\\d])';	# Year 1
+			 $re3='(.)';	# Any Single Character 1
+			 $re4='(.)';	# Any Single Character 2
+			 $re5='((?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Sept|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?))';	# Month 1
+
+			 if ($c=preg_match_all ("/".$re1.$re2.$re3.$re4.$re5."/is", $txt, $matches))
+			 {
+					 $year1=$matches[1][0];
+					 $c1=$matches[2][0];
+					 $c2=$matches[3][0];
+					 $month1=$matches[4][0];
+					 echo '<br>' . 'date is ' . $year1 . ", " . $month1 . '<br>';
+					 $date = date_create_from_format('Y, F, j', $year1 . ", " . $month1 . ", " . "1");
+					 echo '<br>' . 'converted date' . date_format($date, 'Y-m-d') . '<br>';
+	 					return date_format($date, 'Y-m-d');
+			 }
+
+			} catch (Exception $ex) {
+				echo 'Could not convert ' . $rawDate . '\t' . $ex->getMessage() . '<br>';
+				return '10/30/2016';
 			}
 		}
 	}

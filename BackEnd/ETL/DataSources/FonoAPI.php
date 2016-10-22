0f84696c852e700db2744854b12a006176010e38
Tanay <tanay@webexposure.ca>
2016-10-21 21:49:58 -0400

    FonoAPI specification title sanitization

--- BackEnd/ETL/DataSources/FonoAPI.php
+++ BackEnd/ETL/DataSources/FonoAPI.php
@@ -1,138 +1,145 @@
 <?php
 
-class FonoAPI
-{
-	static $_ApiKey = null;
-	static $debug = false;
+    class FonoAPI {
+        static $_ApiKey = null;
+        static $debug = false;
 
-	static $_ApiUrl = "https://fonoapi.freshpixl.com/v1/";
+        static $_ApiUrl = "https://fonoapi.freshpixl.com/v1/";
 
-	function __construct($ApiKey, $url = null)
-	{
-		if($url != null) {
-			self::$_ApiUrl = $url;
-		}
+        function __construct($ApiKey, $url = null)
+        {
+            if($url != null) {
+                self::$_ApiUrl = $url;
+            }
 
-		self::$_ApiKey = $ApiKey;
-	}
+            self::$_ApiKey = $ApiKey;
+        }
 
-	/**
-	 * Instaniate Self
-	 */
-	public static function init($ApiKey) {
-		$f = new self($ApiKey);
-		return $f;
-	}
+        /**
+         * Instaniate Self
+         */
+        public static function init($ApiKey) {
+            $f = new self($ApiKey);
+            return $f;
+        }
 
-	/**
-	 * Turn Debug On
-	 */
-	public static function debug($ApiKey) {
-		$f = new self($ApiKey);
-		$f::$debug = true;
-		return $f;
-	}
+        /**
+         * Turn Debug On
+         */
+        public static function debug($ApiKey) {
+            $f = new self($ApiKey);
+            $f::$debug = true;
+            return $f;
+        }
 
-	/**
-	 *
-	 * Gets Device Data Object from fonoapi.freshpixl.com
-	 *
-	 * @param      $device
-	 * @param null $position
-	 * @param null $brand
-	 *
-	 * @return mixed
-	 * @throws \Exception
-	 */
+        /**
+         *
+         * Gets Device Data Object from fonoapi.freshpixl.com
+         *
+         * @param      $device
+         * @param null $position
+         * @param null $brand
+         *
+         * @return mixed
+         * @throws \Exception
+         */
 
-	public static function getDevice($device, $position = null, $brand = null){
-		$url = self::$_ApiUrl . "getdevice";
+        public static function getDevice($device, $position = null, $brand = null){
+            $url = self::$_ApiUrl . "getdevice";
 
-		$postData = array(
-			'brand' => trim($brand),
-			'device' => trim($device),
-			'position' => $position,
-			'token' => self::$_ApiKey
-			 );
+            $postData = array(
+                'brand' => trim($brand),
+                'device' => trim($device),
+                'position' => $position,
+                'token' => self::$_ApiKey
+            );
 
-		$result = json_decode(self::sendPostData($url, $postData));
+            $result = json_decode(self::sendPostData($url, $postData));
 
-		return handlePostResult($result);
-	}
+            return handlePostResult($result);
+        }
 
-	/**
-	 *
-	 * Gets Latest Device Data Objects from fonoapi.freshpixl.com
-	 *
-	 * @param null $brand
-	 * @param 100 $limit
-	 *
-	 * @return mixed
-	 * @throws \Exception
-	 */
+        /**
+         *
+         * Gets Latest Device Data Objects from fonoapi.freshpixl.com
+         *
+         * @param null $brand
+         * @param 100 $limit
+         *
+         * @return mixed
+         * @throws \Exception
+         */
 
-	public static function getLatest($brand = null, $limit = 100){
-		$url = self::$_ApiUrl . "getlatest";
+        public static function getLatest($brand = null, $limit = 100){
+            $url = self::$_ApiUrl . "getlatest";
 
-		$postData = array(
-			'brand' => trim($brand),
-			'limit' => $limit,
-			'token' => self::$_ApiKey
-		);
+            $postData = array(
+                'brand' => trim($brand),
+                'limit' => $limit,
+                'token' => self::$_ApiKey
+            );
 
-		$result = json_decode(self::sendPostData($url, $postData));
-		return $result;
-		return handlePostResult($result);
-	}
+            $result = json_decode(self::sendPostData($url, $postData));
+            return $result;
+            return handlePostResult($result);
+        }
 
-	static function handlePostResult($result) {
-		if (isset($result->status)) {
-			$innerException ="";
+        static function handlePostResult($result) {
+            if (isset($result->status)) {
+                $innerException ="";
 
-			if(self::$debug){
-				$innerException = " | <strong>innerException</strong> : " . $result->innerException;
-			}
+                if(self::$debug){
+                    $innerException = " | <strong>innerException</strong> : " . $result->innerException;
+                }
 
-			throw new Exception($result->message . $innerException);
-		}else {
-			return $result;
-		}
-	}
+                throw new Exception($result->message . $innerException);
+            }else {
+                return self::sanitizeSpecTitle($result);
+            }
+        }
 
-	/**
-	 * Sends Post Data to the Server
-	 *
-	 * @param $url
-	 * @param $post
-	 *
-	 * @return mixed
-	 */
-	static function sendPostData($url, $post){
+        // Updates specification titles to ensure compatibility with main data source
+        static function sanitizeSpecTitle($result) {
+            foreach($result as $device) {
+                $device->primary = $device->primary_;
+                $device->loudspeaker = $device->loudspeaker_;
+            }
+        }
 
-		try {
+        /**
+         * Sends Post Data to the Server
+         *
+         * @param $url
+         * @param $post
+         *
+         * @return mixed
+         */
+        static function sendPostData($url, $post){
 
-			$rUrl = "http".(!empty($_SERVER['HTTPS'])?"s":""). "://" .$_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			curl_setopt($ch, CURLOPT_REFERER, $rUrl);
-			$result = curl_exec($ch);
+            try {
 
-			if (FALSE === $result)
-				throw new Exception(curl_error($ch), curl_errno($ch));
+                $rUrl = "http".(!empty($_SERVER['HTTPS'])?"s":""). "://" .$_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+                $ch = curl_init($url);
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+                curl_setopt($ch, CURLOPT_REFERER, $rUrl);
+                $result = curl_exec($ch);
 
-			curl_close($ch);
-			return $result;
+                if (FALSE === $result)
+                    throw new Exception(curl_error($ch), curl_errno($ch));
 
-		} catch (Exception $e) {
+                curl_close($ch);
+                return $result;
 
-			$result["status"] = $e->getCode();
-			$result["message"] = "Curl Failed" ;
-			$result["innerException"] = $e->getMessage();
+            } catch (Exception $e) {
 
-			return json_encode($result);
-		}
-	}
-}
+                $result["status"] = $e->getCode();
+                $result["message"] = "Curl Failed" ;
+                $result["innerException"] = $e->getMessage();
+
+                return json_encode($result);
+            }
+        }
+    }
